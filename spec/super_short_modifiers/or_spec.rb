e85b807516803370e21049771ee8773abcef56ca
pasberth <pasberth@gmail.com>
2012-04-26 02:34:37 +0900

    created or spec

---
+++ spec/super_short_modifiers/or_spec.rb
@@ -0,0 +1,15 @@
+describe "OR modifier." do
+  subject do
+    Class.new do
+      include SuperShort::ObjectMethods
+      include SuperShort::Modifiable
+      attr_accessor :example
+    end.new
+  end
+  
+  its(:example) { should be_nil }
+  it { subject.example = "lisp"; subject.get_or_set :example, "LISP"; subject.example.should == "lisp" }
+  it { subject.get_or_set :example, "lisp"; subject.example.should == "lisp" }
+  it { subject.example = true; subject.get_or_set :example, "lisp"; subject.example.should == true }
+  it { subject.example = false; subject.get_or_set :example, "lisp"; subject.example.should == false }
+end
